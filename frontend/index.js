e46bbb0217a488fa54cd00f2ce49617e87f5ee3b
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2023-10-20 02:06:33 -0400

    task5and6

--- frontend/index.js
+++ frontend/index.js
@@ -74,6 +74,7 @@ function moduleProject2() {
     let isDown = evt.key === keys.down
     let isLeft = evt.key === keys.left
     let isRight = evt.key === keys.right
+    let isSpaceBar = evt.key === keys.space
 
     let targeted = document.querySelector('.targeted')
     
@@ -102,9 +103,30 @@ function moduleProject2() {
     }
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
-    
+    else if (isSpaceBar) {
+      let mosquito = targeted.firstChild
 
-    // 👉 TASK 5 - End the game 👈
+      if (mosquito && mosquito.dataset.status === 'alive') {
+        mosquito.dataset.status === 'dead'
+        mosquito.parentElement.style.backgroundColor = 'red'
+      }
+
+      // 👉 TASK 5 - End the game 👈
+      let liveMosquitos = document.querySelectorAll("[data-status=alive]")
+      if (!liveMosquitos.length) {
+        let elapsed = getTimeElapsed()
+        document.querySelector('p.info').textContent = 
+        `Extermination completed in ${elapsed / 1000} seconds!`
+
+        let restartBtn = document.createElement('button')
+        restartBtn.textContent = 'Restart'
+        restartBtn.addEventListener('click', () => {
+          location.reload()
+        })
+        document.querySelector('h2'). insertAdjacentElement('beforeend', restartBtn)
+      }
+    }
+
   })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
